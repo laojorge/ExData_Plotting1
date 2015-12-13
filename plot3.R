2c5936cbe6e66b12387ed4ba43d08528d3272483
laojorge <laojorge@gmail.com>
2015-12-13 19:04:55 -0200

    Commit plot3.R

--- plot3.R
+++ plot3.R
@@ -8,4 +8,3 @@ plot(data2_3$datetime, data2_3$Sub_metering_1, type="l", ylab="Energy Submeterin
 lines(data2_3$datetime, data2_3$Sub_metering_2, type="l", col="red")
 lines(data2_3$datetime, data2_3$Sub_metering_3, type="l", col="blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, lwd=2.5)
-dev.off()
